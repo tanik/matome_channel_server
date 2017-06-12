125610fc2676ea2c6f0dcf1ee5e852dd1cb97616
tanimoto <naito.boonboon@gmail.com>
2017-06-13 07:16:24 +0900

    change locale to ja

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'rack-cors', require: 'rack/cors'
 gem 'kaminari'
 gem 'devise_token_auth'
 gem 'omniauth'
+gem 'devise-i18n'
 gem 'redis', '~> 3.0'
 gem 'redis-namespace'
 gem 'sidekiq'
@@ -35,6 +36,7 @@ gem 'elasticsearch-model'
 gem 'elasticsearch-rails'
 gem 'rest-client'
 gem 'newrelic_rpm'
+gem 'rails-i18n', '~> 5.0.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module MatomeChannelServer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
